e8b2afbeb48743446c1ee4ddee7eadb039004e64
Paillaugue bruno <paillaugue.bruno@gmail.com>
2025-04-03 18:02:15 +0200

    Ajout boucle principale // manque functions input et data

--- main.py
+++ main.py
@@ -23,8 +23,38 @@ def is_find(word):
             return False
     return True
 
-#Permet l'affichage du mot sous forme semi-caché
-def print_word(word):
-    for char in word:
-        print(char if char in charFound else "_", end=" ")
-    print("\n\n")
+#Boucle principale du jeu
+rep = ""
+print("Bienvenue dans le jeu du Pendu ! Commençons :)\n")
+while(rep == ""):
+    ("Choix d'un nouveau mot . . .")
+    if data == []:
+        print("Oups, vous avez épuisée la base de données...")
+        exit()
+    wordToFind = pick_random_word(data)
+    charFound = [wordToFind[0]]
+    charTested.clear()
+    charTested.append(wordToFind[0])
+    restingLives = 6
+    while(not is_find(wordToFind)):
+        print_word(wordToFind)
+        char = prop_letter()
+        if(is_in_word(char, wordToFind)):
+            print("\nBien vu ! Cette lettre est dans le mot à trouver")
+        else:
+            restingLives -=1
+            print(f"\nDommage, cette lettre n'est pas dans le mot\nIl vous reste {restingLives} vies")
+            if(restingLives == 0):
+                print(f"Vous n'avez plus de vie ! Le mot était : {wordToFind}")
+                rep = input("Tapez 'entrée' pour continuer à jouer, ou autre chose si vous voulez arrêter.")
+                if rep == "":
+                    break
+                else:
+                    print("Fermeture du jeu.")
+                    exit()
+    if(restingLives >0):
+        print(f"\nFélicitation vous avez trouvé le mot : {wordToFind}\n")
+        rep = input("Tapez 'entrée' pour continuer à jouer, ou autre chose si vous voulez arrêter.")
+        if rep != "":
+            print("Fermeture du jeu.")
+            exit()
