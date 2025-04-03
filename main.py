5e38677d43f15cb2580de6d13bf40956c80a6ea3
Aurélien Duarte Costa <aurelien.duarte-costa@univ-lyon2.fr>
2025-04-03 21:04:57 +0200

    Ajout d’une fonction permettant d’extraire un mot pseudo-aléatoirement.

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ file = "data.txt"
 with open(file) as dataFile:
     data = [line.strip() for line in dataFile.readlines()]
 
+#Récupère un mot aléatoirement dans les données
+def pick_random_word(data):
+    i = random.randint(0,len(data)-1)
+    return data.pop(i)
