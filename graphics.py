45e2421d3d3295c12b0755bd807c5af770cecc70
Gregg <80894851+p4bbl0@users.noreply.github.com>
2025-04-04 14:48:31 +0200

    améliorer play_menu

--- graphics.py
+++ graphics.py
@@ -4,7 +4,41 @@ lstButton = []
 
 #Pas encore implémenté
 def play_menu():
-    return 0
+    playMenu = Tk()
+
+    playMenu.config(bg="#ffcd00")
+    playMenu.geometry("1080x720")
+
+    frame = Frame(playMenu, bg="#ffcd00")
+    frame.pack(expand=YES)
+
+    title = Label(frame, text=hidden_word("TEST"), font=("Courrier",40, "bold"), fg="white", bg="#ffcd00", pady=10)
+    title.pack()
+
+    canva = Canvas(frame, width=300, height=300, bg="#ffcd00", highlightthickness=0)
+    canva.pack()
+
+    letters = Frame(frame, bg="#ffcd00")
+    letters.pack(expand=YES)
+
+    #Création des boutons:
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    global lstButton
+    row= 0
+    column = 0
+    for char in alphabet:
+        button = Button(letters, text=char, width=5, height=2, padx=2, pady=2)
+        button.grid(row=row, column= column)
+        lstButton.append(button)
+        column += 1
+        if column ==10:
+            row +=1
+            if(row == 2):
+                column = 2
+            else:
+                column =0
+    playMenu.mainloop()
+    
 def start_menu():
     global window
     window.config(bg="#ffcd00")
@@ -24,3 +58,4 @@ start_menu()
 #Fonction factice à récupérer dans le main
 def hidden_word(word):
     return "T _ _ T"
+
