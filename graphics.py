376511ef92dcc89f162fb693fc044696ce530839
Gregg <80894851+p4bbl0@users.noreply.github.com>
2025-04-04 14:45:45 +0200

    ajoute la fonction start_menu

--- graphics.py
+++ graphics.py
@@ -3,3 +3,15 @@ from tkinter import *
 #Pas encore implémenté
 def play_menu():
     return 0
+def start_menu():
+    global window
+    window.config(bg="#ffcd00")
+    window.geometry("1080x720")
+    frame = Frame(window, bg="#ffcd00")
+    frame.pack(expand=YES)
+    title = Label(frame, text="Jeu du Pendu", font=("Courrier",40, "bold"), fg="white", bg="#ffcd00", pady=75)
+    title.pack()
+
+    button = Button(frame, bg="#ffcd00", fg="white", font=("Courrier",20, "bold"), text="Jouer", relief=SUNKEN, command=play_menu)
+    button.pack(fill=X)
+    window.mainloop()
